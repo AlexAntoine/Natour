328de9a3d2dd6b0a447f83513d5dd26625246d6f
Alex Antoine <antoinealex1234@gmail.com>
2024-06-04 14:18:03 -0400

    routers for tours, middleware included

---
+++ routes/tourRouter.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const fs = require('fs')
+const {getAllTours,createATour,getOneTour,updateTour,deleteTour,checkID, checkBody} = require('../controllers/toursController')
+const router = express.Router();
+
+router.param('id',checkID);
+
+router.route('/')
+.get(getAllTours)
+.post(checkBody,createATour);
+
+router.route('/:id')
+.get(getOneTour)
+.patch(updateTour)
+.delete(deleteTour);
+
+module.exports = router;
