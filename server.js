e046f73370a267ff2fe24721544514b2890c1e57
AlexAntoine <anotoinealex1234@gmail.com>
2024-06-21 01:34:00 -0400

    added local db and tours model

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
+const {local} = require('./db/database');
+const Tours = require('./model/tours');
 const app = require('./app');
 
 const port = 3000;
+
+local();
 
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`)
